86cba8db15a77acd6a56181391ec0cbe36d05790
Owlypixel <owlypixel@gmail.com>
2018-10-26 22:15:59 +0300

    initial commit

---
+++ CoreCats/CatsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace CoreCats
+{
+    public class CatsService
+    {
+        public HttpClient Client { get; set; }
+
+        public CatsService (HttpClient client)
+        {
+            client.BaseAddress = new System.Uri("https://thecatapi.com/");
+            
+            client.DefaultRequestHeaders.Add("User-Agent", "HttpClient-Sample");
+
+            this.Client = client;
+        }
+    }
+}
---
+++ CoreCats/Pages/TypedCats.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace CoreCats.Pages
+{
+    public class TypedCatsModel : PageModel
+    {
+        private CatsService _cService;
+
+        public string CatImageURL { get; set; }
+
+        public TypedCatsModel(CatsService service)
+        {
+            _cService = service;
+        }
+        public async Task OnGetAsync()
+        {
+            String TimeStamp = DateTime.Now.ToString();
+            var query = "/api/images/get?format=json&type=gif&timestamp=" + TimeStamp;
+            var APIResult = await _cService.Client.GetStringAsync(query);
+            List<CatApiResponse> DeserializedResult = JsonConvert.DeserializeObject<List<CatApiResponse>>(APIResult);
+            CatImageURL = DeserializedResult[0].Url;
+        }
+        public class CatApiResponse
+        {
+            public string id { get; set; }
+            public string Url { get; set; }
+            public string SourceURL { get; set; }
+        }
+    }
+}
